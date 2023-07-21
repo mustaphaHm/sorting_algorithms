9663f110a83c65448f519e5c19bb17ae44158f49
MustaphaHm <mustapha.houmam@gmail.com>
2023-07-21 11:33:56 -0700

    initial commit , with task 0 and readme file

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -36,17 +36,21 @@ void printArray(int *array, size_t size)
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
+	bool swapped;
 
 	for (i = 0; i < size; i++)
 	{
+		swapped = false;
 		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
 				swap(&array[j], &array[j + 1]);
 				printArray(array, size);
-
+				swapped = true;
 			}
 		}
+		if (swapped == false)
+			break;
 	}
 }
