a75d390e80ad09f7c2aab9b2c3e07f7eef817e2d
MustaphaHm <mustapha.houmam@gmail.com>
2023-07-21 11:39:11 -0700

    Correct betty issues

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -24,8 +24,8 @@ void bubble_sort(int *array, size_t size)
 	size_t i, j;
 	bool swapped;
 
-	if (array == NULL || size <2)
-		return;i
+	if (array == NULL || size < 2)
+		return;
 	for (i = 0; i < size; i++)
 	{
 		for (j = 0; j < size - i - 1; j++)
