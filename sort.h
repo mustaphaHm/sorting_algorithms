d87460670364880ee6ca0869e817489bf0883b6c
MustaphaHm <mustapha.houmam@gmail.com>
2023-07-22 12:45:47 -0700

    forgot to push the header file

--- sort.h
+++ sort.h
@@ -19,4 +19,5 @@ void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void swap(int *el1, int *el2);
 void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
 #endif
