2c2540a14c655bc7c69ed3ba51768db572413c44
MustaphaHm <mustapha.houmam@gmail.com>
2023-07-22 11:53:43 -0700

    change the algorithm again for effeciency issues

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -6,28 +6,38 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *curr, *tmp;
+	listint_t *curr, *tmp, *next;
 
 	if (*list == NULL || (*list)->next == NULL)
 		return;
-	curr = (*list)->next;
-	while (curr != NULL)
+
+	next = (*list)->next;
+	while (next != NULL)
 	{
-		tmp = curr;
-		while (tmp->prev != NULL && tmp->n < tmp->prev->n)
+		curr = next;
+		for (tmp = curr->prev; tmp != NULL; tmp = tmp->prev)
 		{
-			tmp->prev->next = tmp->next;
-			if (tmp->next != NULL)
-				tmp->next->prev = tmp->prev;
-			tmp->next = tmp->prev;
-			tmp->prev = tmp->prev->prev;
-			if (tmp->prev == NULL)
-				*list = tmp;
-			else
-				tmp->prev->next = tmp;
-			tmp->next->prev = tmp;
-			print_list(*list);
+			if (tmp->n > curr->n)
+			{
+				curr->prev->next = curr->next;
+				if (curr->next != NULL)
+					curr->next->prev = curr->prev;
+				curr->next = tmp;
+				if (tmp->prev != NULL)
+				{
+					tmp->prev->next = curr;
+					curr->prev = tmp->prev;
+				}
+				else
+				{
+					*list = curr;
+					curr->prev = NULL;
+				}
+				tmp->prev = curr;
+				print_list(*list);
+				tmp = curr;
+			}
 		}
-		curr = curr->next;
+		next = next->next;
 	}
 }
